ccf408f2a865a62cf7eed14316304d9c62ace77c
Justin Geeslin <justin@geesl.in>
2020-01-20 10:58:00 -0600

    Removing media functions from the shim

--- s_code-shim.js
+++ s_code-shim.js
@@ -2,18 +2,18 @@ s = typeof s !== "undefined" ? s : {};
 s.Media = typeof s.Media !== "undefined" ? s.Media : {};
 s.Media.autoTrack = true;
 
-s.Media.play = typeof s.Media.play !== "undefined" ? s.Media.play : function() {
-  console.log('s Object Shim: Play')
-}
-
-s.Media.pause = function() {
-  console.log('s Object Shim: Pause')
-}
-
-s.Media.stop = function() {
-  console.log('s Object Shim: Stop')
-}
-
-s.Media.close = function() {
-  console.log('s Object Shim: Close')
-}
+// s.Media.play = typeof s.Media.play !== "undefined" ? s.Media.play : function() {
+//   console.log('s Object Shim: Play')
+// }
+//
+// s.Media.pause = function() {
+//   console.log('s Object Shim: Pause')
+// }
+//
+// s.Media.stop = function() {
+//   console.log('s Object Shim: Stop')
+// }
+//
+// s.Media.close = function() {
+//   console.log('s Object Shim: Close')
+// }
